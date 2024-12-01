ded6eca0db0ee12885f5cc7f73c20e860324dfe1
Javvq <167478687+Javvq@users.noreply.github.com>
2024-11-30 21:55:08 -0500

    Update ProductoCard.js

--- servipet_frontend/src/paginas/proyecto/productos/ProductoCard.js
+++ servipet_frontend/src/paginas/proyecto/productos/ProductoCard.js
@@ -1,26 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const ProductoCard = ({productos}) => {
-    return (
 
-        <section className="bg-light text-white py-5">
-            <div className="container">
-                <div className="row">
-                    {productos.map((producto) => (
-                        <div key={producto.idDto} className="col-md-3">
-                            <div className="card text-center text-black bg-secondary mb-3">
-                                <img src={`data:image/jpeg;base64,${producto.imagenProductoDto}`} className="card-img-top" alt={producto.nombreProductoDto} />
-                                <div className="card-body">
-                                    <h5 className="card-title">{producto.nombreProductoDto}</h5>
-                                    <p className="card-text">{producto.precioProductoDto} $</p>
-                                    <Link to={`/producto/detalles/${producto.idDto}`} className="btn btn-dark">Comprar</Link>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+const ProductoCard = ({ productos }) => {
+  return (
+    <section className="bg-light py-5">
+      <div className="container">
+        <div className="row">
+          {productos.map((producto) => (
+            <div key={producto.idDto} className="col-md-3">
+              <div className="card product-card text-center mb-4">
+                <div className="card-img-wrapper">
+                  <img
+                    src={`data:image/jpeg;base64,${producto.imagenProductoDto}`}
+                    className="card-img-top"
+                    alt={producto.nombreProductoDto}
+                  />
                 </div>
+                <div className="card-body">
+                  <h5 className="card-title">{producto.nombreProductoDto}</h5>
+                  <p className="card-text product-price">
+                    {producto.precioProductoDto} $
+                  </p>
+                  <Link
+                    to={`/producto/detalles/${producto.idDto}`}
+                    className="btn btn-dark"
+                  >
+                    Comprar
+                  </Link>
+                </div>
+              </div>
             </div>
-        </section>
-    )
-    }
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default ProductoCard;
